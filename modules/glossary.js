673eb259f60901eda4e7aba2b646fc77ebc4d3bd
Steffen <steffen@baumgart.biz>
2021-07-14 21:13:48 +0200

    fixed role lookup, added glossary

---
+++ modules/glossary.js
@@ -0,0 +1,90 @@
+const rp = require("request-promise-native");
+const Discord = require("discord.js");
+const utils = require("../utils");
+const log = utils.getLogger('glossary');
+
+class BotcGlossaryLoader {
+  constructor(commandChar) {
+    this.cc = commandChar;
+    this.commands = {
+      glossary: {
+        aliases: ['define'],
+        inline: true,
+        description: "Search the glossary for a definition",
+        help: '',
+        examples: ["glossary drunk", "define dead"]
+      },
+    };
+    this.wikiPage = "https://bloodontheclocktower.com/wiki/Glossary";
+    this.wikiApi = "https://bloodontheclocktower.com/wiki/api.php?action=query&prop=categories|revisions&rvprop=content&format=json&rvslots=*&titles=Glossary";
+    this.definitions = new Map();
+    this.fetchDefinitions();
+  }
+
+  getCommands() {
+    return this.commands;
+  }
+
+  /**
+   * Fetch the glossary from the BOTC wiki
+   * @returns {Promise<Object>}
+   */
+  async fetchDefinitions() {
+    const body = await rp({url: this.wikiApi, json: true});
+    if (body.query && body.query.pages["366"]) {
+      const page = body.query.pages["366"];
+      const glossary = page.revisions[0].slots.main['*'].replace(/\n +\n/,"\n\n").split("\n\n").map(e =>
+        (e.match(/^'''(.*?):?''':? (.*)$/) || []).splice(1,2));
+      log.info("loaded " + glossary.length + " definitions");
+      this.definitions = new Map(glossary.map(e => [e[0].trim().toLowerCase(), {
+        title: e[0],
+        definition: e[1]
+      }]));
+    } else throw new Error('not found');
+  }
+
+  // generate the embed
+  generateEmbed({ title, definition }) {
+    // footer
+    let footer = this.wikiPage;
+
+    // instantiate embed object
+    return new Discord.MessageEmbed({
+      title: 'Glossary: ' + title,
+      description: definition,
+      footer: {text: footer},
+      url: this.wikiPage,
+      color: 0x2096FF
+    });
+  }
+
+  /**
+   * Handle an incoming message
+   * @param command
+   * @param parameter
+   * @param msg
+   * @returns {Promise}
+   */
+  handleMessage(command, parameter, msg) {
+    const search = parameter.toLowerCase();
+    // no search param, no lookup
+    if (!search) return;
+    // fetch data from Glossary
+    if(this.definitions.has(search)) {
+      // generate embed
+      const embed = this.generateEmbed(this.definitions.get(search));
+      return msg.channel.send('', {embed});
+    } else {
+      let description = 'No glossary entry matched `' + search + '`.';
+      return msg.channel.send('', {
+        embed: new Discord.MessageEmbed({
+          title: 'Error',
+          description,
+          color: 0xff0000
+        })
+      });
+    }
+  }
+}
+
+module.exports = BotcGlossaryLoader;
--- modules/role.js
+++ modules/role.js
@@ -6,124 +6,126 @@ const log = utils.getLogger('role');
 const cheerio = require("cheerio");
 
 class BotcRoleLoader {
-    constructor(commandChar) {
-        this.cc = commandChar;
-        this.commands = {
-            role: {
-                aliases: ['char'],
-                inline: true,
-                description: "Search for an official Blood on the Clocktower role by name",
-                help: '',
-                examples: ["role washerwoman", "char spy"]
-            },
-        };
-        this.wikiUrl = "http://bloodontheclocktower.com/wiki/";
-        this.wikiApi = this.wikiUrl + "api.php?action=query&prop=categories|revisions&rvprop=content&format=json&titles=";
-        this.wikiImage = this.wikiUrl + 'Special:FilePath/';
-        this.wikiSearch = this.wikiUrl + 'api.php?action=query&list=search&srwhat=title&format=json&srsearch=';
-        // embed border colors depending on category
-        this.colors = {
-            "Category:Demons": 0xEC0804,
-            "Category:Fabled": 0xECCB21,
-            "Category:Minions": 0x9F0400,
-            "Category:Outsiders": 0x183EFF,
-            "Category:Townsfolk": 0x2096FF,
-            "Category:Travelers": 0xc519ff,
-        };
-    }
+  constructor(commandChar) {
+    this.cc = commandChar;
+    this.commands = {
+      role: {
+        aliases: ['char'],
+        inline: true,
+        description: "Search for an official Blood on the Clocktower role by name",
+        help: '',
+        examples: ["role washerwoman", "char spy"]
+      },
+    };
+    this.wikiUrl = "http://bloodontheclocktower.com/wiki/";
+    this.wikiApi = this.wikiUrl + "api.php?action=query&prop=categories|revisions&rvprop=content&format=json&rvslots=*&titles=";
+    this.wikiImage = this.wikiUrl + 'Special:FilePath/';
+    this.wikiSearch = this.wikiUrl + 'api.php?action=query&list=search&srwhat=title&format=json&srsearch=';
+    // embed border colors depending on category
+    this.colors = {
+      "Category:Demons": 0xEC0804,
+      "Category:Fabled": 0xECCB21,
+      "Category:Minions": 0x9F0400,
+      "Category:Outsiders": 0x183EFF,
+      "Category:Townsfolk": 0x2096FF,
+      "Category:Travelers": 0xc519ff,
+    };
+  }
 
-    getCommands() {
-        return this.commands;
-    }
+  getCommands() {
+    return this.commands;
+  }
 
-    // generate the embed
-    generateEmbed(page, command) {
-        // generate embed title and description text
-        const title = page.title;
-        let team = 'No team';
-        let edition = 'No edition'
-        let color;
-        page.categories.forEach(({title}) => {
-           if(!!this.colors[title]) {
-               team = title.replace(/Category:/,'');
-               color = this.colors[title];
-           } else {
-               edition = title.replace(/Category:/,'');
-           }
-        });
+  // generate the embed
+  generateEmbed(page, command) {
+    // generate embed title and description text
+    const title = page.title;
+    let team = 'No team';
+    let edition = 'No edition'
+    let color;
+    page.categories.forEach(({title}) => {
+      if (!!this.colors[title]) {
+        team = title.replace(/Category:/, '');
+        color = this.colors[title];
+      } else {
+        edition = title.replace(/Category:/, '');
+      }
+    });
 
-        const $ = cheerio.load(page.revisions[0]['*']);
-        const content = $('.columns').text().replace(/==.*?==/ig,'###').replace(/[\n\t]/g,'').split('###')
-        let description = '**' + edition + ' / ' + team + '**\n';
-        description += content[0];
-        description += '```' + content[1].substr(1,content[1].length - 2) + '```';
+    const $ = cheerio.load(page.revisions[0].slots.main['*']);
+    const content = $('.columns').text().replace(/==.*?==/ig, '###').replace(/[\n\t]/g, '').split('###')
+    let description = '**' + edition + ' / ' + team + '**\n';
+    description += content[0];
+    description += '```' + content[1].substr(1, content[1].length - 2) + '```';
 
-        const image = $('.columns').text().match(/\[\[File:(.*?)\|/)[1];
+    const image = $('.columns').text().match(/\[\[File:(.*?)\|/)[1];
 
-        // footer
-        let footer = "Use "+this.cc+"help to get a list of available commands.";
+    // footer
+    let footer = "Use " + this.cc + "help to get a list of available commands.";
 
-        // instantiate embed object
-        return new Discord.MessageEmbed({
-            title,
-            description,
-            footer: {text: footer},
-            url: this.wikiUrl + encodeURIComponent(title),
-            color,
-            thumbnail: image ? {url: this.wikiImage + image} : null
-        });
-    }
+    // instantiate embed object
+    return new Discord.MessageEmbed({
+      title,
+      description,
+      footer: {text: footer},
+      url: this.wikiUrl + encodeURIComponent(title),
+      color,
+      thumbnail: image ? {url: this.wikiImage + image} : null
+    });
+  }
 
-    /**
-     * Fetch the role from the BOTC wiki
-     * @param role
-     * @returns {Promise<Object>}
-     */
-    async getRole(role) {
-        const search = await rp({url: this.wikiSearch + encodeURIComponent('*'+role+'*'), json: true});
-        if (search.query && search.query.search.length) {
-            search.query.search.sort((a, b) => b.wordcount - a.wordcount);
-            const title = search.query.search[0].title;
-            const body = await rp({url: this.wikiApi + encodeURIComponent(title), json: true});
-            if(body.query && !body.query.pages['-1']) {
-                const page = body.query.pages[Object.keys(body.query.pages)[0]];
-                if (page.categories && page.categories.some(({title}) => !!this.colors[title])) {
-                    return page;
-                }
-            }
+  /**
+   * Fetch the role from the BOTC wiki
+   * @param role
+   * @returns {Promise<Object>}
+   */
+  async getRole(role) {
+    const search = await rp({url: this.wikiSearch + encodeURIComponent('*' + role + '*'), json: true});
+    if (search.query && search.query.search.length) {
+      const {title, pageid} = search.query.search.filter(({snippet, wordcount}) =>
+        !snippet.match(/#redirect/i) && wordcount > 100).shift();
+      const body = await rp({url: this.wikiApi + encodeURIComponent(title), json: true});
+      if (body.query && body.query.pages[pageid]) {
+        const page = body.query.pages[pageid];
+        if (page.categories && page.categories.some(({title}) => !!this.colors[title])) {
+          return page;
         }
-        throw new Error('not found');
+      }
     }
+    throw new Error('not found');
+  }
 
-    /**
-     * Handle an incoming message
-     * @param command
-     * @param parameter
-     * @param msg
-     * @returns {Promise}
-     */
-    handleMessage(command, parameter, msg) {
-        const role = parameter.toLowerCase();
-        // no card name, no lookup
-        if (!role) return;
-        // fetch data from API
-        this.getRole(role).then(page => {
-            // generate embed
-            const embed = this.generateEmbed(page, command)
-            return msg.channel.send('', {embed});
-        }).catch(err => {
-            console.error(err);
-            let description = 'No roles matched `'+role+'`.';
-            if (err.statusCode === 503) {
-                description = 'Wiki is currently offline, please try again later.'
-            }
-            return msg.channel.send('', {embed: new Discord.MessageEmbed({
-                title: 'Error',
-                description,
-                color: 0xff0000
-            })});
-        });
-    }
+  /**
+   * Handle an incoming message
+   * @param command
+   * @param parameter
+   * @param msg
+   * @returns {Promise}
+   */
+  handleMessage(command, parameter, msg) {
+    const role = parameter.toLowerCase();
+    // no role name, no lookup
+    if (!role) return;
+    // fetch data from API
+    this.getRole(role).then(page => {
+      // generate embed
+      const embed = this.generateEmbed(page, command)
+      return msg.channel.send('', {embed});
+    }).catch(err => {
+      console.error(err);
+      let description = 'No roles matched `' + role + '`.';
+      if (err.statusCode === 503) {
+        description = 'Wiki is currently offline, please try again later.'
+      }
+      return msg.channel.send('', {
+        embed: new Discord.MessageEmbed({
+          title: 'Error',
+          description,
+          color: 0xff0000
+        })
+      });
+    });
+  }
 }
 
 module.exports = BotcRoleLoader;
